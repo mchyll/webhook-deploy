a933ce00c5a93c3cf29ac5dc0cd09c07acbcda3c
Magnus Conrad Hyll <magnus@hyll.no>
2019-08-06 16:38:52 +0200

    WIP

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
+import os
+import yaml
 import hmac
 import time
 import multiprocessing as mp
 
 
-def deploy(secret, payload):
-    pass
+def load_secret() -> bytes:
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'secret.txt')) as f:
+        return f.readline().strip().encode('utf-8')
 
 
-def verify_signature(payload: str, signature: str) -> bool:
+def load_config() -> dict:
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.yml')) as f:
+        return yaml.load(f)
+
+
+def verify_signature(payload: str, signature: str, secret: bytes) -> bool:
     payload_bytes = payload.encode('utf-8')
-    key_bytes = 'KEY'.encode('utf-8')  # TODO get key from somewhere safe
-    digest = hmac.new(key_bytes, payload_bytes, 'sha1')
+    digest = hmac.new(secret, payload_bytes, 'sha1')
     expected_signature = 'sha1=' + digest.hexdigest()
     return hmac.compare_digest(expected_signature, signature)
 
 
-def test():
-    with open('secret.txt', 'a+') as f:
-        f.seek(0)
-        print(f.readline())
-        time.sleep(10)
+def do_deploy(config, id, payload):
+    # Sequence:
+    # Check if repo name is in config
+    # Check if pushed ref is branch for that repo in config
+    # Make a temp directory
+    # Clone repo into that directory (user running this webapp must have an ssh key registered on github?)
+    # Build the source code in the cloned repo from a build config
+    # Copy all (or only built files) files to the destination dir in config
+    # Delete temp dir
+    # Log all of this
+    # Store the log and stuff somewhere, identified by the delivery id
+    repo_name = payload['repository']['full_name']
+    for deployment in config['deployments']:
+        pass
+    pushed_ref = payload['ref']
+    pass
 
 
 if __name__ == '__main__':
-    print(verify_signature('testæøå', 'lol'))
-    print(f'hehe :P {time.time()}')
-
-    mp.set_start_method('spawn')
-    p1 = mp.Process(target=test)
-    p2 = mp.Process(target=test)
-    p1.start()
-    p2.start()
+    secret = load_secret()
+    print(verify_signature('testæøå', 'lol', secret))
--- server.py
+++ server.py
@@ -1,13 +1,40 @@
 from aiohttp import web
+import json
+from main import verify_signature, load_config, load_secret
 
 
-async def handle(request: web.Request):
-    name = request.match_info.get('name', "Anonymous")
-    text = "Hello, " + name
-    return web.Response(text=text)
+__config = load_config()
+__secret = load_secret()
+
+
+async def github_webhook_handle(req: web.Request):
+    try:
+        payload = await req.text()
+        signature = req.headers['X-Hub-Signature']
+        delivery_id = req.headers['X-GitHub-Delivery']
+        event_type = req.headers['X-GitHub-Event']
+
+        if verify_signature(payload, signature, __secret):
+            if event_type == 'ping':
+                # TODO: Check against config, log?
+                pass
+            elif event_type == 'push':
+                print(f'Yee. Signature: {signature}, delivery id: {delivery_id}, payload: {payload}')
+                return web.Response(status=200, text=f'Yee. Signature: {signature}, delivery id: {delivery_id}, payload: {payload}')
+        else:
+            # TODO: Warn log about invalid signature
+            pass
+
+    except json.decoder.JSONDecodeError as ex:
+        print(f'JSON decode error: {ex.msg}')
+        return web.Response(status=400)
+
+    except KeyError as ex:
+        print(f'Key error: {ex}')
+        return web.Response(status=400)
+
 
 app = web.Application()
-app.add_routes([web.get('/', handle),
-                web.get('/{name}', handle)])
+app.add_routes([web.post('/github-webhook', github_webhook_handle)])
 
 web.run_app(app)
